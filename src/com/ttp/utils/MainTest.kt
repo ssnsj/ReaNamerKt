e72629c0118538799669e52af90a550ff10b4577
Ian <developer.kss@gmail.com>
2017-09-09 07:30:05 +0900

    assertRename

--- src/com/ttp/utils/MainTest.kt
+++ src/com/ttp/utils/MainTest.kt
@@ -8,40 +8,44 @@ import java.util.*
 class MainTest {
     @Test
     fun renameBasic() {
-        assertEquals(File("res/drawable-mdpi/a.png"), rename(File(""), File("a.png")))
-        assertEquals(File("abc/res/drawable-mdpi/a.png"), rename(File("abc"), File("a.png")))
-        assertEquals(File("/abc/res/drawable-mdpi/a.png"), rename(File("/abc"), File("a.png")))
+        assertRename(expected = "res/drawable-mdpi/a.png", source = "", name = "a.png")
+        assertRename("abc/res/drawable-mdpi/a.png", "abc", "a.png")
+        assertRename("/abc/res/drawable-mdpi/a.png", "/abc", "a.png")
+    }
+
+    private fun assertRename(expected: String, source: String, name: String) {
+        assertEquals(File(expected), rename(File(source), File(name)))
     }
 
     @Test
     fun rename2x() {
-        assertEquals(File("res/drawable-xhdpi/a.png"), rename(File(""), File("a@2x.png")))
-        assertEquals(File("sdfd/res/drawable-xhdpi/b.png"), rename(File("sdfd"), File("b@2x.png")))
-        assertEquals(File("cccd/res/drawable-xhdpi/dfsf.png"), rename(File("cccd"), File("dfsf@2x.png")))
+        assertRename("res/drawable-xhdpi/a.png", "", "a@2x.png")
+        assertRename("sdfd/res/drawable-xhdpi/b.png", "sdfd", "b@2x.png")
+        assertRename("cccd/res/drawable-xhdpi/dfsf.png", "cccd", "dfsf@2x.png")
     }
 
     @Test
     fun rename3x() {
-        assertEquals(File("res/drawable-xxxhdpi/a.png"), rename(File(""), File("a@3x.png")))
-        assertEquals(File("bbad/res/drawable-xxxhdpi/b.png"), rename(File("bbad"), File("b@3x.png")))
-        assertEquals(File("dfdfi/dfjk/res/drawable-xxxhdpi/dfsf.png"), rename(File("dfdfi/dfjk"), File("dfsf@3x.png")))
+        assertRename("res/drawable-xxxhdpi/a.png", "", "a@3x.png")
+        assertRename("bbad/res/drawable-xxxhdpi/b.png", "bbad", "b@3x.png")
+        assertRename("dfdfi/dfjk/res/drawable-xxxhdpi/dfsf.png", "dfdfi/dfjk", "dfsf@3x.png")
     }
 
     private fun rename(source: File, file: File): File {
-        var convertTable = Hashtable<String, String>()
-        convertTable.put("@2x", "xhdpi")
-        convertTable.put("@3x", "xxxhdpi")
+        val replaceTable = Hashtable<String, String>()
+        replaceTable.put("@2x", "xhdpi")
+        replaceTable.put("@3x", "xxxhdpi")
 
-        for (item in convertTable.keys) {
-            if (item in file.name) {
-                val targetFolder = source.resolve("res/drawable-${convertTable.get(item)}")
-                val newFile = file.name.replace(item, "")
+        for (key in replaceTable.keys) {
+            if (key in file.name) {
+                val targetFolder = source.resolve("res/drawable-${replaceTable.get(key)}")
+                val newFile = file.name.replace(key, "")
 
                 return targetFolder.resolve(newFile)
             }
         }
 
-        return source.resolve("res/drawable-mdpi/").resolve(file)
+        return source.resolve("res/drawable-mdpi").resolve(file)
     }
 
 }
